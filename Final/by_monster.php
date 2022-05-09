4c998a8724a9213e57e0330cfbf20a51bbd71e47
beatrix-walter <98893142+beatrix-walter@users.noreply.github.com>
2022-05-09 12:25:12 -0400

    Update by_monster.php

--- Final/by_monster.php
+++ Final/by_monster.php
@@ -3,37 +3,7 @@
 
 <head>
  <style>
-
-h1 {
-  color: dimgray;
-  font-family: American Typewriter, serif;
-  font-size: 200%;
-}
-    h2 {
-        text-align: center;
-        color: dimgray;
-        font-family: American Typewriter, serif;
-        font-size: 120%;
-    }
-    body {
-        border: 1px solid black;
-        width: 1024px;
-        margin: auto;
-        text-align: center;
-        color: dimgray;
-        font-family: American Typewriter, serif;
-        font-size: 150%;
-    }
-p {
-  color: dimgrey;
-  font-family: American Typewriter, serif;
-  font-size: 80%;
-}
-button {
-    color: dimgrey;
-  font-family: American Typewriter, serif;
-  font-size: 80%;   
-    }
+<link rel="stylesheet" href="final.css">
 </style>
 </head>
     
